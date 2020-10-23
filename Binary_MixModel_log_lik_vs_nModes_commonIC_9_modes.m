e8ba1c547a88229ef522fd99f50a3020a3a1b2c1
Anirudh Krishnan <50084862+anirudhkrishnan2718@users.noreply.github.com>
2020-10-22 21:03:24 -0400

    sync desktop long sim done

--- Binary_MixModel_log_lik_vs_nModes_commonIC_9_modes.m
+++ Binary_MixModel_log_lik_vs_nModes_commonIC_9_modes.m
@@ -11,7 +11,8 @@ input2 = load("CommonIC_9_Folds_3_EMIter_500_log_lik_vs_nModes_25_5_50.mat");
 
 %% using mean and standard deviation for plotting
 all_nModes = [input1.n_modes_values input2.n_modes_values];
-all_log_lik_raw = transpose([input1.avg_log_lik_overFolds; input2.avg_log_lik_overFolds]);
+all_log_lik_raw_full = transpose([input1.avg_log_lik_overFolds; input2.avg_log_lik_overFolds]);
+% all_log_lik_raw = all_log_lik_raw_full(:, 1:3)
 
 %% using median and interpolated 68th percentile for plotting
 
@@ -25,6 +26,13 @@ Y_ebar_pos = avg_log_lik_percentile_mu_sigma_upper - avg_log_lik_medians;
 Y_ebar_neg = avg_log_lik_medians - avg_log_lik_percentile_mu_sigma_lower;
 X_ebar_pos = zeros(1, 10);
 X_ebar_neg = zeros(1, 10);
+
+%% using raw data oints for a scatter plot
+
+all_nModes_scatter = repmat(all_nModes, 9, 1);
+z1 = reshape(all_nModes_scatter, [], 1);
+all_log_lik_raw_scatter = all_log_lik_raw;
+z2 = reshape(all_log_lik_raw_scatter, [], 1);
 
 %% plotting avg lok lik vs nModes using mean, stddev
 figure
@@ -69,3 +77,24 @@ xlabel('Number of modes', 'Interpreter','latex', 'FontSize', 14)
 ylabel('Average log likelihood from model', 'Interpreter','latex', 'FontSize', 14)
 % legend({'test chunk1', 'test chunk2', 'test chunk3'}, 'Interpreter','latex', 'FontSize', 12)
 legend({'median and mu +- sigma percentiles for data points and errorbar'}) 
+
+%% plotting avg lok lik vs nModes raw data scatter plot
+figure
+hold on
+grid on
+box on
+
+avg_log_lik_chunks = mean(all_log_lik_raw, 1);
+avg_log_lik_stddev = std(all_log_lik_raw,0, 1);
+
+
+scatter(z1, z2, 10, [0 0.5 0], 'filled')
+% 
+% scatter(avg_log_lik(1, :), avg_log_lik(2, :), 'red', 'filled')
+% scatter(avg_log_lik(1, :), avg_log_lik(3, :), 'green', 'filled')
+% scatter(avg_log_lik(1, :), avg_log_lik(4, :), 'blue', 'filled')
+title('Average log likelihood using 3 fold cross validation and 9 distinct initial conditions as a function of number of modes - raw data scatter', 'Interpreter','latex', 'FontSize', 20)
+xlabel('Number of modes', 'Interpreter','latex', 'FontSize', 14)
+ylabel('Average log likelihood from model', 'Interpreter','latex', 'FontSize', 14)
+% legend({'test chunk1', 'test chunk2', 'test chunk3'}, 'Interpreter','latex', 'FontSize', 12)
+legend({'raw data points for each distinct IC'})
